daaf3dd47121b3df6d57331c54f5c1da07514c48
annalo2912 <anastasialopes2912@gmail.com>
2025-11-22 12:46:24 +0530

    solving the port issues

--- app.py
+++ app.py
@@ -1399,4 +1399,4 @@ def update_cluster_chart(_):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))  # use Render's PORT if set, else 8050
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run( host='0.0.0.0', port=port)
